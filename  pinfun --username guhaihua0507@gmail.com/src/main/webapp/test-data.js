be8706181e576d7b56280536beff24be14a4063e
guhaihua0507@gmail.com <guhaihua0507@gmail.com@a1a91132-694e-6756-0834-829a8cd16ee1>
2012-06-10 09:45:47 +0000

    update

---  pinfun --username guhaihua0507@gmail.com/src/main/webapp/test-data.js
+++  pinfun --username guhaihua0507@gmail.com/src/main/webapp/test-data.js
@@ -1,188 +1,124 @@
 var data = [];
 (function() {
 	data.push({
-		id: 1,
-		iconImage: 'pic/icon_image.jpg',
-		height: 128,
-		image: "pic/image.jpg",
-		description: "descriptions hahaha!",
-		likeCount: 100,
-		commentsCount: 4,
-		repinsCount: 10,
-		user: {
-			id: 'ghh',
-			name: "guhaihua",
-			icon: "pic/user_icon.jpg"
+		id : 1,
+		iconImage : 'pic/icon_image.jpg',
+		height : 128,
+		image : "pic/image.jpg",
+		description : "descriptions hahaha!",
+		likeCount : 100,
+		commentsCount : 4,
+		repinsCount : 10,
+		userId : "ghh",
+		userName : "guhaihua",
+		userIcon : "pic/user_icon.jpg",
+		topic : {
+			id : 'haha',
+			name : "topic name"
 		},
-		topic: {
-			id: 'haha',
-			name: "topic name"
-		},
-		comments : [
-		  {
+		comments : [ {
 			id : 'comment id1',
 			content : "very good!",
-			user : {
-				id : 'user1',
-				name : "user name1",
-				icon : "pic/user_icon1.jpg"
-			}
-		  }
-		]
+			userId : "user1",
+			userName : "user name 1",
+			userIcon : "pic/user_icon1.jpg",
+		} ]
 	});
-	
+
 	data.push({
-		id: 1,
-		iconImage: 'pic/icon_image.jpg',
-		height: 128,
-		image: "pic/image.jpg",
-		description: "descriptions hahaha!",
-		likeCount: 100,
-		commentsCount: 4,
-		repinsCount: 10,
-		user: {
-			id: 'ghh',
-			name: "guhaihua",
-			icon: "pic/user_icon.jpg"
+		id : 1,
+		iconImage : 'pic/icon_image.jpg',
+		height : 128,
+		image : "pic/image.jpg",
+		description : "descriptions hahaha!",
+		likeCount : 100,
+		commentsCount : 4,
+		repinsCount : 10,
+		userId : "ghh",
+		userName : "guhaihua",
+		userIcon : "pic/user_icon.jpg",
+		topic : {
+			id : 'haha',
+			name : "topic name"
 		},
-		topic: {
-			id: 'haha',
-			name: "topic name"
-		},
-		comments : [
-		  {
+		comments : [ {
 			id : 'comment id1',
 			content : "very good!",
-			user : {
-				id : 'user1',
-				name : "user name1",
-				icon : "pic/user_icon1.jpg"
-			}
-		  }
-		]
+			userId : "user1",
+			userName : "user name 1",
+			userIcon : "pic/user_icon1.jpg",
+		} ]
 	});
-	
 	data.push({
-		id: 1,
-		iconImage: 'pic/icon_image.jpg',
-		height: 128,
-		image: "pic/image.jpg",
-		description: "descriptions hahaha!",
-		likeCount: 100,
-		commentsCount: 4,
-		repinsCount: 10,
-		user: {
-			id: 'ghh',
-			name: "guhaihua",
-			icon: "pic/user_icon.jpg"
+		id : 1,
+		iconImage : 'pic/icon_image.jpg',
+		height : 128,
+		image : "pic/image.jpg",
+		description : "descriptions hahaha!",
+		likeCount : 100,
+		commentsCount : 4,
+		repinsCount : 10,
+		userId : "ghh",
+		userName : "guhaihua",
+		userIcon : "pic/user_icon.jpg",
+		topic : {
+			id : 'haha',
+			name : "topic name"
 		},
-		topic: {
-			id: 'haha',
-			name: "topic name"
-		},
-		comments : [
-		  {
+		comments : [ {
 			id : 'comment id1',
 			content : "very good!",
-			user : {
-				id : 'user1',
-				name : "user name1",
-				icon : "pic/user_icon1.jpg"
-			}
-		  }
-		]
+			userId : "user1",
+			userName : "user name 1",
+			userIcon : "pic/user_icon1.jpg",
+		} ]
 	});
-	
 	data.push({
-		id: 1,
-		iconImage: 'pic/icon_image.jpg',
-		height: 128,
-		image: "pic/image.jpg",
-		description: "descriptions hahaha!",
-		likeCount: 100,
-		commentsCount: 4,
-		repinsCount: 10,
-		user: {
-			id: 'ghh',
-			name: "guhaihua",
-			icon: "pic/user_icon.jpg"
+		id : 1,
+		iconImage : 'pic/icon_image.jpg',
+		height : 128,
+		image : "pic/image.jpg",
+		description : "descriptions hahaha!",
+		likeCount : 100,
+		commentsCount : 4,
+		repinsCount : 10,
+		userId : "ghh",
+		userName : "guhaihua",
+		userIcon : "pic/user_icon.jpg",
+		topic : {
+			id : 'haha',
+			name : "topic name"
 		},
-		topic: {
-			id: 'haha',
-			name: "topic name"
-		},
-		comments : [
-		  {
+		comments : [ {
 			id : 'comment id1',
 			content : "very good!",
-			user : {
-				id : 'user1',
-				name : "user name1",
-				icon : "pic/user_icon1.jpg"
-			}
-		  }
-		]
+			userId : "user1",
+			userName : "user name 1",
+			userIcon : "pic/user_icon1.jpg",
+		} ]
 	});
-	
 	data.push({
-		id: 1,
-		iconImage: 'pic/icon_image.jpg',
-		height: 128,
-		image: "pic/image.jpg",
-		description: "descriptions hahaha!",
-		likeCount: 100,
-		commentsCount: 4,
-		repinsCount: 10,
-		user: {
-			id: 'ghh',
-			name: "guhaihua",
-			icon: "pic/user_icon.jpg"
+		id : 1,
+		iconImage : 'pic/icon_image.jpg',
+		height : 128,
+		image : "pic/image.jpg",
+		description : "descriptions hahaha!",
+		likeCount : 100,
+		commentsCount : 4,
+		repinsCount : 10,
+		userId : "ghh",
+		userName : "guhaihua",
+		userIcon : "pic/user_icon.jpg",
+		topic : {
+			id : 'haha',
+			name : "topic name"
 		},
-		topic: {
-			id: 'haha',
-			name: "topic name"
-		},
-		comments : [
-		  {
+		comments : [ {
 			id : 'comment id1',
 			content : "very good!",
-			user : {
-				id : 'user1',
-				name : "user name1",
-				icon : "pic/user_icon1.jpg"
-			}
-		  }
-		]
-	});
-	
-	data.push({
-		id: 1,
-		iconImage: 'pic/icon_image.jpg',
-		height: 128,
-		image: "pic/image.jpg",
-		description: "descriptions hahaha!",
-		likeCount: 100,
-		commentsCount: 4,
-		repinsCount: 10,
-		user: {
-			id: 'ghh',
-			name: "guhaihua",
-			icon: "pic/user_icon.jpg"
-		},
-		topic: {
-			id: 'haha',
-			name: "topic name"
-		},
-		comments : [
-		  {
-			id : 'comment id1',
-			content : "very good!",
-			user : {
-				id : 'user1',
-				name : "user name1",
-				icon : "pic/user_icon1.jpg"
-			}
-		  }
-		]
+			userId : "user1",
+			userName : "user name 1",
+			userIcon : "pic/user_icon1.jpg",
+		} ]
 	});
 })();
