6bd5b3f5421cee448d5c3d6d118b370b03feb516
guhaihua0507@gmail.com <guhaihua0507@gmail.com@a1a91132-694e-6756-0834-829a8cd16ee1>
2012-06-15 05:29:49 +0000

    refactor

--- trunk/ pinfun --username guhaihua0507@gmail.com/src/main/webapp/js/pin.js
+++ trunk/ pinfun --username guhaihua0507@gmail.com/src/main/webapp/js/pin.js
@@ -1,9 +1,9 @@
 var PinData = function(data) {
 	this.id = null;
-	this.iconImage = null;
+	this.thumb = null;
+	this.image = null;
 	this.picWidth = 0;
 	this.picHeight = 0;
-	this.image = null;
 	this.description = '';
 	this.likeCount = 0;
 	this.commentsCount = 0;
@@ -17,7 +17,7 @@ var PinData = function(data) {
 	};
 	this.comments = [];
 	
-	this.viewUI = null;
+	this.container = null;
 	this.init(data);
 };
 
@@ -26,67 +26,91 @@ PinData.prototype = {
 		for (var p in data) {
 			this[p] = data[p];
 		}
-		this.viewUI = $("<div class='pin'></div>").attr('id', this.id);
+		this.container = $("<div class='pin'></div>").attr('id', this.id);
 		
-		var pinHolder = $("<div class='pinHolder'></div>");
-		pinHolder.append("<a href=" + this.image + " class='PinImage ImgLink'><img src='" + this.iconImage + "' height=" + this.picHeight + " alt='" + this.description + "' class='PinImageImg'></a>");
-		this.viewUI.append(pinHolder);
-		
-		this.viewUI.append("<p class='description'>" + this.description + "</p>");
-		var state = $('<p/>').addClass('stats colorless').appendTo(this.viewUI);
+		this.renderImage();
+		this.renderDescription();
+		this.renderState();
+		this.renderCategory();
+		this.renderComments();
+	},
+	
+	renderImage : function() {
+		var holder = $("<div class='pinHolder'></div>");
+		var a = $("<a></a>").addClass("PinImage ImgLink").attr("href", this.image);
+		var img = $("<img class='PinImageImg'/>").attr("src", this.thumb).attr("height", this.picHeight).attr("alt", this.description);
+		a.append(img);
+		holder.append(a);
+		this.container.append(holder);
+	},
+	
+	renderDescription: function() {
+		this.container.append($("<p class='description'></p>").append(this.description));
+	},
+	
+	renderState : function() {
+		var state = $("<p/>").addClass("stats colorless");
 		state.append($("<span/>").text(this.likeCount + ' likes '));
 		state.append($("<span/>").text(this.commentsCount + ' comments '));
 		state.append($("<span/>").text(this.repinsCount + ' repins '));
-		
-		$("<div class='convo attribution clearfix'></div>").append("<a href='" + this.userId + "' class='ImgLink'><img src='"+ this.userIcon +"'></a>")
-			.append("<a href='" + this.userId + "'>"+ this.userName + "</a> <a href='" + this.topic.id + "'>"+this.topic.name+"</a>").appendTo(this.viewUI);
-		
-		var comets = $("<div class='comments colormuted'></div>").appendTo(this.viewUI);
+		this.container.append(state);
+	},
+	
+	renderCategory : function() {
+		var holder = $("<div class='convo attribution clearfix'></div>");
+		$("<a class='ImgLink'></a>").attr("href", this.userId).append("<img src='" + this.userIcon + "'>").appendTo(holder);
+		$("<a></a>").attr("href", this.userId).append(this.userName).appendTo(holder);
+		holder.append(" onto ");
+		$("<a></a>").attr("href", this.topic.id).append(this.topic.name).appendTo(holder);
+		this.container.append(holder);
+
+	},
+	
+	renderComments : function() {
+		var commHolder = $("<div class='comments colormuted'></div>");
 		for (var i = 0; i < this.comments.length; i++) {
 			var commObj = $("<div class='comment convo clearfix'></div>").attr('id', this.comments[i].id);
-			commObj.append("<a href='" + this.comments[i].userId + "' class='ImgLink'><img src='"+ this.comments[i].userIcon +"' class='profile user_image'></a>");
-			commObj.append("<a href='" + this.comments[i].userId + "'>" + this.comments[i].userName + "</a> " + this.comments[i].content);
-			comets.append(commObj);
+			var a1 = $("<a class='ImgLink'></a>").attr("href", this.comments[i].userId).append($("<img class='profile user_image'/>").attr("src", this.comments[i].userIcon));
+			var a2 = $("<a></a>").attr("href", this.comments[i].userId).append(this.comments[i].userName);
+			commObj.append(a1);
+			commObj.append(a2);
+			commObj.append(" " + this.comments[i].content);
+			commHolder.append(commObj);
 		}
+		this.container.append(commHolder);
 	},
 	
-	loadImage : function() {
-		
-	},
-	
-	loadState : function() {
-		
-	},
-	
-	loadTopic : function() {
-		
-	},
-	
-	loadComments : function() {
-		
+	height : function() {
+		return this.container.height();
 	}
 };
 
 var DataLayout = {
-	columnCount : 4,
-	dataArray : [],
-	columns : [],
+	minColumnCount : 4,
 	pinWidth: 192,
 	columnPadding : 30,
 	topPadding : 15,
 	columnMargin: 15,
+	columnCount : 4,
+	dataArray : [],
+	columns : [],
 	
 	init : function(param) {
+		this.loadContext();
 		var left = 0;
 		for ( var i = 0; i < this.columnCount; i++) {
-			this.columns.push({
-				height : 0,
-				left : left
-			});
+			this.columns.push({height : 0, left : left });
 			left += this.pinWidth + this.columnPadding + this.columnMargin;
 		}
 	},
 
+	loadContext : function() {
+		var columnWidth = this.pinWidth + this.columnPadding + this.columnMargin;
+		this.columnCount = Math.max(this.minColumnCount, parseInt(($(window).width() + this.columnMargin) / columnWidth));
+		var containerWidth = (this.columnCount * columnWidth - this.columnMargin);
+		$("#ColumnContainer").width(containerWidth);
+	},
+	
 	loadData : function(d) {
 		for (var i = 0; i < d.length; i++) {
 			this.pin(new PinData(d[i]));
@@ -102,13 +126,13 @@ var DataLayout = {
 			}
 		}
 
-		pindata.viewUI.css({
+		pindata.container.css({
 			top : column.height + 'px',
 			left : column.left + 'px'
 		});
 
-		$("#ColumnContainer").append(pindata.viewUI);
-		column.height += pindata.viewUI.height() + this.topPadding + this.columnMargin;
+		$("#ColumnContainer").append(pindata.container);
+		column.height += pindata.height() + this.topPadding + this.columnMargin;
 		this.dataArray.push(pindata);
 	},
 	
