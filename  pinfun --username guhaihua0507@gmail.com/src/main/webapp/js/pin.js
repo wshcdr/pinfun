3d76519e1a5a10bc4cc6722415440d92dff0e3fa
guhaihua0507@gmail.com <guhaihua0507@gmail.com@a1a91132-694e-6756-0834-829a8cd16ee1>
2012-06-16 03:48:28 +0000

    develop pin fun main page

---  pinfun --username guhaihua0507@gmail.com/src/main/webapp/js/pin.js
+++  pinfun --username guhaihua0507@gmail.com/src/main/webapp/js/pin.js
@@ -16,59 +16,62 @@ var PinData = function(data) {
 		name : null
 	};
 	this.comments = [];
-	
-	this.container = null;
+
+	this.ui = null;
 	this.init(data);
 };
 
 PinData.prototype = {
 	init : function(data) {
-		for (var p in data) {
+		this._loadParameter(data);
+		this.ui = $("<div class='pin'></div>").attr('id', this.id);
+		this._renderImage();
+		this._renderDescription();
+		this._renderState();
+		this._renderCategory();
+		this._renderComments();
+	},
+
+	_loadParameter : function(data) {
+		for ( var p in data) {
 			this[p] = data[p];
 		}
-		this.container = $("<div class='pin'></div>").attr('id', this.id);
-		
-		this.renderImage();
-		this.renderDescription();
-		this.renderState();
-		this.renderCategory();
-		this.renderComments();
 	},
 	
-	renderImage : function() {
+	_renderImage : function() {
 		var holder = $("<div class='pinHolder'></div>");
 		var a = $("<a></a>").addClass("PinImage ImgLink").attr("href", this.image);
 		var img = $("<img class='PinImageImg'/>").attr("src", this.thumb).attr("height", this.picHeight).attr("alt", this.description);
 		a.append(img);
 		holder.append(a);
-		this.container.append(holder);
+		this.ui.append(holder);
 	},
-	
-	renderDescription: function() {
-		this.container.append($("<p class='description'></p>").append(this.description));
+
+	_renderDescription : function() {
+		this.ui.append($("<p class='description'></p>").append(this.description));
 	},
-	
-	renderState : function() {
+
+	_renderState : function() {
 		var state = $("<p/>").addClass("stats colorless");
 		state.append($("<span/>").text(this.likeCount + ' likes '));
 		state.append($("<span/>").text(this.commentsCount + ' comments '));
 		state.append($("<span/>").text(this.repinsCount + ' repins '));
-		this.container.append(state);
+		this.ui.append(state);
 	},
-	
-	renderCategory : function() {
+
+	_renderCategory : function() {
 		var holder = $("<div class='convo attribution clearfix'></div>");
 		$("<a class='ImgLink'></a>").attr("href", this.userId).append("<img src='" + this.userIcon + "'>").appendTo(holder);
 		$("<a></a>").attr("href", this.userId).append(this.userName).appendTo(holder);
 		holder.append(" onto ");
 		$("<a></a>").attr("href", this.topic.id).append(this.topic.name).appendTo(holder);
-		this.container.append(holder);
+		this.ui.append(holder);
 
 	},
-	
-	renderComments : function() {
+
+	_renderComments : function() {
 		var commHolder = $("<div class='comments colormuted'></div>");
-		for (var i = 0; i < this.comments.length; i++) {
+		for ( var i = 0; i < this.comments.length; i++) {
 			var commObj = $("<div class='comment convo clearfix'></div>").attr('id', this.comments[i].id);
 			var a1 = $("<a class='ImgLink'></a>").attr("href", this.comments[i].userId).append($("<img class='profile user_image'/>").attr("src", this.comments[i].userIcon));
 			var a2 = $("<a></a>").attr("href", this.comments[i].userId).append(this.comments[i].userName);
@@ -77,71 +80,110 @@ PinData.prototype = {
 			commObj.append(" " + this.comments[i].content);
 			commHolder.append(commObj);
 		}
-		this.container.append(commHolder);
+		if (this.commentsCount > this.comments.length) {
+			commHolder.append($("<a class='all comment convo clearfix'></a>").attr("href", this.image).text("All " + this.commentsCount + " comments..."));
+		}
+		this.ui.append(commHolder);
 	},
-	
+
 	height : function() {
-		return this.container.height();
+		return this.ui.height();
 	}
 };
 
 var DataLayout = {
 	minColumnCount : 4,
-	pinWidth: 192,
+	pinWidth : 192,
 	columnPadding : 30,
 	topPadding : 15,
-	columnMargin: 15,
+	columnMargin : 15,
 	columnCount : 4,
 	dataArray : [],
-	columns : [],
-	
+	colArray : [],
+
 	init : function(param) {
-		this.loadContext();
-		var left = 0;
-		for ( var i = 0; i < this.columnCount; i++) {
-			this.columns.push({height : 0, left : left });
-			left += this.pinWidth + this.columnPadding + this.columnMargin;
-		}
+		this.setupContext();
 	},
 
-	loadContext : function() {
+	setupContext : function() {
+		/*load width*/
+		this._setupLayoutWidth();
+		this._setupColumns();
+	},
+
+	_setupLayoutWidth : function() {
 		var columnWidth = this.pinWidth + this.columnPadding + this.columnMargin;
 		this.columnCount = Math.max(this.minColumnCount, parseInt(($(window).width() + this.columnMargin) / columnWidth));
 		var containerWidth = (this.columnCount * columnWidth - this.columnMargin);
 		$("#ColumnContainer").width(containerWidth);
 	},
 	
-	loadData : function(d) {
-		for (var i = 0; i < d.length; i++) {
-			this.pin(new PinData(d[i]));
+	_setupColumns : function() {
+		this.colArray = [];
+		var left = 0;
+		for ( var i = 0; i < this.columnCount; i++) {
+			this.colArray.push({height:0, left:left});
+			left += this.pinWidth + this.columnPadding + this.columnMargin;
 		}
-		this.calculateHeight();
+	},
+
+	resize : function() {
+		var origColumnCount = this.columnCount;
+		this._setupLayoutWidth();
+		if (origColumnCount == this.columnCount) {
+			return;
+		}
+		this._setupColumns();
+		this._showContent();
 	},
 	
-	pin : function(pindata) {
-		var column = this.columns[0];
-		for ( var i = 1; i < this.columns.length; i++) {
-			if (this.columns[i].height < column.height) {
-				column = this.columns[i];
+	loadNewData : function(_jsonArray) {
+		for (var i = 0; i < _jsonArray.length; i++) {
+			var data = new PinData(_jsonArray[i]);
+			this._showPinData(data);
+			this.dataArray.push(data);
+		}
+		this._refreshContainerHeight();
+	},
+	
+	_showContent : function() {
+		$("#ColumnContainer").empty();
+		for (var i = 0; i < this.dataArray.length; i++) {
+			this._showPinData(this.dataArray[i]);
+		}
+		this._refreshContainerHeight();
+	},
+	
+	_showPinData : function(data) {
+		var minCol = this._getMinMaxHeightColumn(false);
+		data.ui.css("top", minCol.height).css("left", minCol.left);
+		$("#ColumnContainer").append(data.ui);
+		minCol.height += data.height() + this.topPadding + this.columnMargin;
+	},
+	
+	_getMinMaxHeightColumn : function(b) {		//false:min height, true:max height
+		var col = this.colArray[0];
+		for (var i = 1; i < this.colArray.length; i++) {
+			if (!b && this.colArray[i].height < col.height) {
+				col = this.colArray[i];
+			}
+			if (b && this.colArray[i].height > col.height) {
+				col = this.colArray[i];
 			}
 		}
-
-		pindata.container.css({
-			top : column.height + 'px',
-			left : column.left + 'px'
-		});
-
-		$("#ColumnContainer").append(pindata.container);
-		column.height += pindata.height() + this.topPadding + this.columnMargin;
-		this.dataArray.push(pindata);
+		return col;
 	},
-	
-	calculateHeight : function() {
-		var maxHeight = 0;
-		for (var i = 0; i < this.columns.length; i++) {
-			maxHeight = Math.max(maxHeight, this.columns[i].height);
-		}
-		$("#ColumnContainer").height(maxHeight);
+
+	_refreshContainerHeight : function() {
+		var maxCol = this._getMinMaxHeightColumn(true);
+		$("#ColumnContainer").height(maxCol.height);
 	}
-	
 };
+
+$(function() {
+	$(document).scroll(function(){
+        if (($(window).scrollTop() + $(window).height()) == $(document).height()) {	//at the bottom
+        	DataLayout.loadNewData(data);
+        }
+    });
+});
